c2992eeed94e2ebba5b3dc333fdad6794a37d2fa
Bryant Overgard <bryant.overgard@timmons.com>
2022-08-02 10:38:44 -0400

    Adding identify to static vector layer. Fixing bug with static vector source

--- src/staticVector.js
+++ src/staticVector.js
@@ -4,35 +4,43 @@ import VectorSource from 'ol/source/Vector';
 import { ConfigurableStyle } from './vectorStyles'
 
 
+import * as identifyUtils from './identifyUtils'
 
 
 
 
 export const generate = (data, core) => {
-    let layers = data.config.value.endpoints.map(endpoint => {
-        
-        let source = new VectorSource({
-          maxZoom: 15,
-        });
+  let layers = data.config.value.endpoints.map(endpoint => {
 
-        source.highlightFeats = {};
+    let source = new VectorSource({
+      maxZoom: 15,
+    });
+
+    source.highlightFeats = {};
 
 
-        let vLayer = new VectorLayer({
-          declutter: true,
-          source: source,
-          zIndex: endpoint.zIndex || 1000
-        });
+    let vLayer = new VectorLayer({
+      declutter: true,
+      source: source,
+      zIndex: endpoint.zIndex || 1000
+    });
 
-        let moo = new ConfigurableStyle(endpoint, source, vtLayer);
+    let moo = new ConfigurableStyle(endpoint, source, vLayer);
 
-        vLayer.style = moo.getStyle;
-        vLayer.setStyle(moo.getStyle)
+    vLayer.style = moo.getStyle;
+    vLayer.setStyle(moo.getStyle)
 
-        vLayer.set('id', data.key);
-        
-        return vLayer;
-      });
+    vLayer.set('id', data.key);
 
-    return layers;
+
+    vLayer.getFeaturesInView = identifyUtils.getFeaturesInView(vLayer, endpoint, core.getMap());
+
+    vLayer.getFeaturesUnderPixel = identifyUtils.getFeaturesUnderPixel(vLayer, endpoint, core.getMap());
+
+    return vLayer;
+  });
+
+  layers.map(a=>a);
+
+  return layers;
 }
